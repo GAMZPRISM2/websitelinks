c033719694b981a0e595b9aec50fdf7f403a70db
GAMZPRISM2 <109406122+GAMZPRISM2@users.noreply.github.com>
2024-03-27 13:50:46 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('link1').addEventListener('click', function(event) {
+  event.preventDefault();
+  window.location.href = 'https://example.com/link1';
+});
+
+document.getElementById('link2').addEventListener('click', function(event) {
+  event.preventDefault();
+  window.location.href = 'https://example.com/link2';
+});
